d0e29eacc6403998e6f143653b65fce2030bd1fb
Kimberly Robasky <krobasky@gmail.com>
2021-03-23 15:03:40 -0400

    added timeout to allow any dependent containers to finish during testing

--- tests/test.sh
+++ tests/test.sh
@@ -2,4 +2,4 @@
 
 export $(cat tests/test.env|grep -v "^#")
 
-docker-compose -f docker-compose.yml -f tests/docker-compose.yml up --build -V --exit-code-from fuse-appliance-template
+docker-compose -f docker-compose.yml -f tests/docker-compose.yml up --build -V  --timeout 600 --exit-code-from fuse-appliance-template
